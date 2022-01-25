790a00f5f0151c428805e8655258c8d70a242f00
Rahul Sarkar <rahulsarkar334@gmail.com>
2022-01-26 01:35:32 +0530

    api integration

--- app/controllers/resumeController.py
+++ app/controllers/resumeController.py
@@ -22,14 +22,18 @@ def ResumeUploadController(files, db: Session):
         db_obj = {}
         stark_base = STARKBase(UploadedResumes)
         s3obj = S3_Helper(env['secret'], env['access'], env['OVERRIDE_S3_ENDPOINT'], env['region'])
-        res = s3obj.upload_s3(files=files, bucket_name=env['RESUME_BUCKET'])
-        if res is True:
-            for file in files:
-                db_obj["name"] = file.filename
-                stark_base.create(db=db, obj_in=db_obj)
-            return {"status": True, "type": "ResumeUpload", "data": "Resume uploaded successfully"}
+        validate_file = is_pdf(files)
+        if validate_file is True:
+            res = s3obj.upload_s3(files=files, bucket_name=env['RESUME_BUCKET'])
+            if res is True:
+                for file in files:
+                    db_obj["name"] = file.filename
+                    stark_base.create(db=db, obj_in=db_obj)
+                return {"status": True, "type": "ResumeUpload", "data": "Resume uploaded successfully"}
+            else:
+                return {"status": False, "type": "ResumeUpload", "data": "Something went wrong"}
         else:
-            return {"status": False, "type": "ResumeUpload", "data": "Something went wrong"}
+            return {"status": False, "type": "ResumeUpload", "data": "File needs to be pdf doc or in docx format"}
     except Exception as er:
         return {"status": False, "type": "ResumeUpload", "data": f"{er}"}
 
@@ -51,3 +55,11 @@ class AlchemyEncoder(json.JSONEncoder):
             return fields
 
         return json.JSONEncoder.default(self, obj)
+
+
+def is_pdf(files: list) -> bool:
+    for file in files:
+        if '.pdf' not in file.filename and '.doc' not in file.filename and '.docx' not in file.filename:
+            return False
+        else:
+            return True
