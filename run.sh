edbab1ac6541b59a12ade4823fd013c079e93b98
rsarkar <rsarkar@vibrenthealth.com>
2022-02-04 22:10:49 -0800

    lin

--- run.sh
+++ run.sh
@@ -10,11 +10,11 @@ export REDIS_ENDPOINT='redis://localhost:6379'
 export RESUME_SOURCE_PATH='s3://datasets/csv_data/resume_data.csv'
 export RESUME_BUCKET='resumes'
 
-export SQLDBURI='mysql+pymysql://root:123456789@localhost/stark_dev'
+export SQLDBURI='postgresql+psycopg2://postgres:123456789@localhost:5432/stark_dev_test'
 export USER='root'
 export PASSWORD='123456789'
 export HOST='127.0.0.1:3306'
 export DB='stark_dev'
 
 echo -e "******************************RION-Stark****************************"
-python main.py main:app --reload --port 5001
+python3 main.py main:app --reload --port 5001
